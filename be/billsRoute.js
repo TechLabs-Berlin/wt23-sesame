cde888b04f036e913b9f7c4d00ed38c36c6b1380
linhvnde <linh.nh.tyty@gmail.com>
2023-03-05 22:06:15 +0100

    adjusted the key-value for bill entry

--- be/billsRoute.js
+++ be/billsRoute.js
@@ -1,19 +1,19 @@
 function addBillRoutes(app) {
     app.get("/bills", (req, res) => {
         res.send([
-            {
-                name: "Edeka",
-                billingDate: new Date(2022, 1, 1).toISOString(),
-                dueAmount: -16.99,
-                logoUrl: "www.edeka.de/logo.png",
-            },
-            {
-                name: "Rewe",
-                billingDate: new Date(2022, 2, 1).toISOString(),
-                dueAmount: -12.4,
-                logoUrl: "www.rewe.de/logo.png",
-            },
-            // more bills
+          {
+            vendor: "Edeka",
+            billingDate: new Date(2022, 1, 1).toISOString(),
+            spentAmount: 16.99,
+            logoUrl: "www.edeka.de/logo.png",
+          },
+          {
+            vendor: "Rewe",
+            billingDate: new Date(2022, 2, 1).toISOString(),
+            spentAmount: 12.4,
+            logoUrl: "www.rewe.de/logo.png",
+          },
+          // more bills
         ]);
     });
 }
