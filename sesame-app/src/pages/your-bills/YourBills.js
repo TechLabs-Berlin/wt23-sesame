f23fedfe1bcb8b83bac08ee71533e3af12fdd313
lutranclan <lu.tran@icloud.com>
2023-03-10 23:40:23 +0100

    Edit Tab bar

--- sesame-app/src/pages/your-bills/YourBills.js
+++ sesame-app/src/pages/your-bills/YourBills.js
@@ -4,19 +4,38 @@ import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import PropTypes from 'prop-types';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { textTransform } from '@mui/system';
 
 
 
-function YourBills({ value, handleChange }) {
+
+  function YourBills({ value, handleChange }) {
+    const [selectedTab, setSelectedTab] = React.useState(0);
+  
+    const handleTabChange = (event, newValue) => {
+      setSelectedTab(newValue);
+    };
+
+    const customTheme = createTheme({
+        typography: {
+          fontSize: 12,
+          // Set your desired font size here
+        },
+      });
+
     return (
         <div>
             <h1 className='app-header'>Your Bills</h1>
-
-            <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
-                <Tabs value={value} onChange={handleChange} centered>
-                    <Tab label="Weekly View" value={0} />
-                    <Tab label="Monthly View" value={1} />
-                    <Tab label="Tab 3" value={2} />
+  
+            <ThemeProvider theme={customTheme}>
+            <Box sx={{ width: '100%', }}>
+                <Tabs value={selectedTab} onChange={handleTabChange} centered>
+                    <Tab label="Weekly View" value={0} sx={{ textTransform: "none" }}/>
+                    <Tab label="Monthly View" value={1} sx={{ textTransform: "none" }}/>
+                    <Tab icon={<CalendarMonthIcon />} aria-label="calendar" />
+                
                 </Tabs>
             </Box>
             <Box sx={{ color: 'black', marginTop: 2, padding: 2, bgcolor: 'background.paper' }}>
@@ -24,7 +43,7 @@ function YourBills({ value, handleChange }) {
                 <p>Chart description goes here</p>
 
             </Box>
-
+            </ThemeProvider>
 
             {/* Page content */}
             <SesameBottomNavigation value={value} onChange={handleChange} />
