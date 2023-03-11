fe0d43a885e2de88fe8faf321c214e77aca2b8e8
lutranclan <lu.tran@icloud.com>
2023-03-11 12:34:03 +0100

    Edit tab bar style, add bg color and border radius

--- sesame-app/src/pages/welcome-splash/WelcomeSplash.js
+++ sesame-app/src/pages/welcome-splash/WelcomeSplash.js
@@ -1,17 +1,16 @@
 //You need to import React at the top of every js file
 import React from 'react';
-import logo from '../../assets/sesame-logo.svg'
-
-
-// Import CSS file
+import logo from '../../assets/sesame-logo.svg';
 import './WelcomeSplash.css';
 
 function WelcomeSplash() {
-    return (
-        <div className="welcome-splash-container">
-            <img src={logo} alt="Logo" className="logo" />
-        </div>
-    );
+
+
+  return (
+    <div className="welcome-splash-container">
+      <img src={logo} alt="Logo" className="logo" />
+    </div>
+  );
 }
 
 export default WelcomeSplash;
--- sesame-app/src/pages/your-bills/YourBills.js
+++ sesame-app/src/pages/your-bills/YourBills.js
@@ -5,9 +5,6 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import PropTypes from 'prop-types';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { textTransform } from '@mui/system';
-
 
 
 
@@ -18,32 +15,24 @@ import { textTransform } from '@mui/system';
       setSelectedTab(newValue);
     };
 
-    const customTheme = createTheme({
-        typography: {
-          fontSize: 12,
-          // Set your desired font size here
-        },
-      });
-
     return (
         <div>
             <h1 className='app-header'>Your Bills</h1>
   
-            <ThemeProvider theme={customTheme}>
-            <Box sx={{ width: '100%', }}>
-                <Tabs value={selectedTab} onChange={handleTabChange} centered>
-                    <Tab label="Weekly View" value={0} sx={{ textTransform: "none" }}/>
-                    <Tab label="Monthly View" value={1} sx={{ textTransform: "none" }}/>
-                    <Tab icon={<CalendarMonthIcon />} aria-label="calendar" />
+            <Box sx={{backgroundColor: '#232D2F', padding: '8px', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                <Tabs value={selectedTab} onChange={handleTabChange} sx={{borderRadius: 2}}>
+                    <Tab label="Weekly View" value={0} sx={{ backgroundColor: 'white', textTransform: "none", fontSize: 12,}}/>
+                    <Tab label="Monthly View" value={1} sx={{ backgroundColor: 'white', textTransform: "none", fontSize: 12, }}/>
+                    <Tab icon={<CalendarMonthIcon />} aria-label="calendar" sx={{backgroundColor: 'white' }}/>
                 
                 </Tabs>
             </Box>
+
             <Box sx={{ color: 'black', marginTop: 2, padding: 2, bgcolor: 'background.paper' }}>
                 <h2>Chart Title</h2>
                 <p>Chart description goes here</p>
 
             </Box>
-            </ThemeProvider>
 
             {/* Page content */}
             <SesameBottomNavigation value={value} onChange={handleChange} />
