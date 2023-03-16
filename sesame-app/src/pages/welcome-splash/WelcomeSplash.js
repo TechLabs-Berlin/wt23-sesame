6c7ea75ff2288908c6e2fc0c62684cad93bd7886
shalvabitsadze <117686913+shalvabitsadze@users.noreply.github.com>
2023-03-16 22:49:46 +0100

    small startup animation/added first two pages

--- sesame-app/src/pages/welcome-splash/WelcomeSplash.js
+++ sesame-app/src/pages/welcome-splash/WelcomeSplash.js
@@ -1,16 +1,34 @@
 //You need to import React at the top of every js file
-import React from 'react';
 import logo from '../../assets/sesame-logo.svg';
 import './WelcomeSplash.css';
+import React, { useState, useEffect } from 'react';
+//npm install --save react-spinners run in terminal if it doesn't install automaticaly
+import GridLoader from "react-spinners/GridLoader";
+//npm install framer-motion  run in terminal if it doesn't install automaticaly
+import { motion } from "framer-motion";
 
 function WelcomeSplash() {
 
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
 
   return (
-    <div className="welcome-splash-container">
-      <img src={logo} alt="Logo" className="logo" />
-    </div>
-  );
+    <section animate={{ y: 100 }} className="welcome-splash-container">
+      {loading ?
+
+        (<GridLoader size={30} color={'#36d698'} loading={loading} />) : (
+
+        <motion.div animate={{ y: -50 }}> <a href="/SignIn"><img src={logo} alt="Logo" className="logo" /></a> </motion.div>)}
+    </section>
+
+  )
 }
+
 
 export default WelcomeSplash;
