d8dc36b9886385191b1926594ec8300a186f8609
lutranclan <lu.tran@icloud.com>
2023-03-17 18:13:23 +0100

    Styled barcharts

--- sesame-app/src/components/BarChart.js
+++ sesame-app/src/components/BarChart.js
@@ -1,4 +1,7 @@
+import { fontSize } from '@mui/system';
 import React from 'react';
+
+//install Recharts in your project, run "npm install recharts"
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 
@@ -13,14 +16,28 @@ const data = [
   ];
 const BarChartComponent = () => {
     return (
-      <BarChart width={600} height={300} data={data}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="day" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="spending" fill="#8884d8" />
-      </BarChart>
+        <ResponsiveContainer width='100%' height={110}>
+            <BarChart data={data}>
+                <CartesianGrid strokeDasharray='3 3' />
+                <XAxis 
+                    dataKey='day' 
+                    style={{
+                        fontSize: '12px', 
+                        fill: 'white',
+                    }}
+                />
+                <YAxis 
+                    width={30}
+                    style={{
+                        fontSize: '12px', 
+                        fill: 'white',
+                    }} 
+                />
+                <Tooltip />
+                {/*<Legend /> */}
+                <Bar dataKey='spending' fill='#3A3BFF' />
+            </BarChart>
+      </ResponsiveContainer>
     );
   
 };
