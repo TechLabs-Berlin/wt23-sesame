51df9a4f5564c5a55f935da2d7c645b834c643d4
linhvnde <linh.nh.tyty@gmail.com>
2023-03-29 23:57:51 +0200

    Del node_modules; modified BE port; started Mongo

--- be/expenseOverviewBarChart.js
+++ be/expenseOverviewBarChart.js
@@ -1,15 +1,13 @@
+const expenditure = require("./expenditure");
+
 function viewExpenseBarChart(app) {
   app.get("/expenseOverviewBarChart", (req, res) => {
-    const data = [
-      { day: "Mon", spending: 200 },
-      { day: "Tue", spending: 100 },
-      { day: "Wed", spending: 300 },
-      { day: "Thu", spending: 400 },
-      { day: "Fri", spending: 150 },
-      { day: "Sat", spending: 250 },
-      { day: "Sun", spending: 350 },
-    ];
-    res.send(data);
+    expenditure
+      .findAllExpenditures()
+      .then((data) => {
+        res.send(data);
+      })
+      .catch((err) => console.error(err));
   });
 }
 module.exports = viewExpenseBarChart;
