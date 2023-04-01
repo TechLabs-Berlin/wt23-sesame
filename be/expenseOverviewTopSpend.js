b31407f4b834c66c656c3b250979ddd713ce16b7
linhvnde <linh.nh.tyty@gmail.com>
2023-04-01 02:14:01 +0200

    Connected Mongo with topexpenses

--- be/expenseOverviewTopSpend.js
+++ be/expenseOverviewTopSpend.js
@@ -1,20 +1,27 @@
+const expenseCat = require("./topexpenses");
+
 function viewExpenseTopSpend(app) {
   app.get("/expenseOverviewTopSpend", (req, res) => {
-    const rows = [
-      {
-        name: "Groceries",
-        amount: 3434,
-      },
-      {
-        name: "Medicines",
-        amount: 1212,
-      },
-      {
-        name: "Cat3",
-        amount: 1212,
-      },
-    ];
-    res.send(rows);
+    expenseCat
+      .findAllExpenseCats()
+      .then((rows) => {
+        res.send(rows);
+      })
+      .catch((err) => console.error(err));
   });
+  // const rows = [
+  //   {
+  //     name: "Groceries",
+  //     amount: 3434,
+  //   },
+  //   {
+  //     name: "Medicines",
+  //     amount: 1212,
+  //   },
+  //   {
+  //     name: "Cat3",
+  //     amount: 1212,
+  //   },
+  // ];
 }
 module.exports = viewExpenseTopSpend;
--- be/index.js
+++ be/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const addBillRoutes = require("./billsRoute");
 const viewExpenseBarChart = require("./expenseOverviewBarChart");
 const viewExpenseTopSpend = require("./expenseOverviewTopSpend");
+
 const app = express();
 const port = 4000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Our express server is up and running");
