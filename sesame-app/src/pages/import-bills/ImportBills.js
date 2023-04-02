8b37ac485b8d11af9c46d7300fb0ef39f64bfc63
lutranclan <lu.tran@icloud.com>
2023-04-02 13:10:38 +0200

    resize qrcode reader with max. width

--- sesame-app/src/pages/import-bills/ImportBills.js
+++ sesame-app/src/pages/import-bills/ImportBills.js
@@ -3,13 +3,8 @@ import SesameBottomNavigation from '../../components/SesameBottomNavigation';
 import PropTypes from 'prop-types';
 import QRCodeWebcam from '../../components/QRCodeWebcam';
 import { Tabs, Tab } from '@mui/material';
-import Box from '@mui/material/Box';
 import QrCodeIcon from '@mui/icons-material/QrCode';
-import TapAndPlayIcon from '@mui/icons-material/TapAndPlay';
 import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
-import Switch from '@mui/material/Switch';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
 import Camera from '../../components/Camera';
 
 
@@ -22,53 +17,35 @@ function ImportBills({ value, handleChange }) {
 
     const singleTabStyles = {
         textTransform: 'none',
-        fontSize: 12,
-        fontWeight: 500,
+        fontSize: '14px',
+        fontWeight: 400,
         color: 'white',
-        minHeight: '32px',
-        height: '50px',
-        minWidth: '70px',
-        padding: '8px',
-        borderRadius: '8px',
-        margin: '10px 30px',
+        width: 'auto',
+        padding: '8px 32px',
+        justifyContent: 'center', 
         
-        '& .MuiSvgIcon-root': {fontSize: '30px'},
+        
+        '& .MuiSvgIcon-root': {fontSize: '24px'},
 
 
         '&.Mui-selected': {
             color: 'white',
-            '& .MuiSvgIcon-root': {color: '#36d698'},
+            '& .MuiSvgIcon-root': {color: '#37FAD3'},
         },
         
     };
 
     const tabsStyles = {
-        minHeight: '50px', 
-        // background: 'white', 
         background: '#232D2F', 
-        borderRadius: '8px', 
-        padding: '4px', 
-        marginBottom: '-41px',
-        // gap: '20px', 
-        '& .MuiTabs-indicator': { backgroundColor: '#36d698'}
+        justifyContent: 'center',
+        margin: '8px',
+        '& .MuiTabs-flexContainer': {justifyContent: 'center',},
+        '& .MuiTabs-indicator': { backgroundColor: '#37FAD3'},
+        
+   
 
     };
 
-    /*const  SwitchLabels = () => {
-        return (
-          <FormGroup >
-            <FormControlLabel
-              style={{ color: 'white',
-                       display: 'flex',
-                       flexDirection: 'row-reverse' ,
-                       paddingTop: '0px'
-                     }}
-
-             control={<Switch defaultUnchecked />}
-             label="Show me QR Code" />
-          </FormGroup>
-        );
-      }*/
     
     return (
         <div>
@@ -77,30 +54,21 @@ function ImportBills({ value, handleChange }) {
                 <h1>Add Bills</h1>
             </section>
 
-            <Box sx={{ display: 'flex', justifyContent: 'center', padding: '8px 30px 24px' }}>
-                <Tabs value={selectedTab} onChange={handleTabChange}  sx={{ ...tabsStyles }}>
-                    <Tab icon={<QrCodeIcon />} label="QR Code"  sx={{ ...singleTabStyles }} />                
-                    <Tab icon={<AddAPhotoIcon />} label="Import" sx={{ ...singleTabStyles }} /> 
-                </Tabs>
-            </Box>
+      
+            <Tabs value={selectedTab} onChange={handleTabChange}  sx={{ ...tabsStyles }}>
+                <Tab icon={<QrCodeIcon />} label="QR Code"  sx={{ ...singleTabStyles }} />
+                <Tab icon={<AddAPhotoIcon />} label="Import" sx={{ ...singleTabStyles }} /> 
+            </Tabs>
             
             
-            <div style= {{display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent:'center',
-                        alignItems: 'center',
-                        margin: '20px 0px 80px',
-                        background: 'white',
-                        //padding: '30px 20px 20px 20px',//
-                        //borderRadius: '16px',//
-                        }}>
                 {selectedTab === 0 && <QRCodeWebcam />}
+            
+                
                 {selectedTab === 1 && <Camera />}
-            </div>
-
-            {/*<SwitchLabels />*/}
+           
                
             <SesameBottomNavigation value={value} onChange={handleChange} />
+
         </div>
     );
 }
