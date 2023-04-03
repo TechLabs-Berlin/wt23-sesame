59a3526d10a11657a47040348020cf3435574702
lutranclan <lu.tran@icloud.com>
2023-04-03 11:55:33 +0200

    set up fetching top spendings

--- sesame-app/src/components/TopFiveSpendings.js
+++ sesame-app/src/components/TopFiveSpendings.js
@@ -11,15 +11,32 @@ function createData(name, spending) {
   return { name, spending};
 }
 
-const rows = [
+/*const rows = [
   createData('Category 1', 159),
   createData('Category 2', 237),
   createData('Category 3', 262),
   createData('Category 4', 305),
   createData('Category 5', 356),
-];
+];*/
 
 function TopFiveSpendings() {
+
+
+  const [rows, setRows] = React.useState([]);
+  
+  React.useEffect(() => {
+    fetch('http://localhost:4000/expenseOverviewTopSpend')
+      .then(response => response.json())
+      .then(data => {
+        const rows = data.map(item => createData(item.name, item.amount));
+        setRows(rows);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);  
+
+
   return (
 
     //The '&' character refers to the current component and '.MuiTableCell-root' is a class name used by the TableCell component.
