1350c31237188398caaf948165778ac581d45c9c
lutranclan <lu.tran@icloud.com>
2023-04-04 03:20:08 +0200

    set new bar chart for monthly spendings

--- sesame-app/src/components/WeeklySpendings.js
+++ sesame-app/src/components/WeeklySpendings.js
@@ -16,12 +16,17 @@ function WeeklySpendings() {
   const [dailyAverage, setDailyAverage] = useState(0)
 
   useEffect(() => {
-    async function fetchSpendings() {
-      const response = await fetch('/api/spendings'); //need to replace url
-      const data = await response.json();
-      setTotalSpending(data.totalSpending);
-      setDailyAverage(data.dailyAverage);
+    const fetchSpendings = async () => {
+      try {
+        const response = await fetch('/api/spendings'); //need to replace url
+        const responseData = await response.json();
+        console.log(responseData);
+        setTotalSpending(responseData.totalSpending);
+        setDailyAverage(responseData.dailyAverage);
+      } catch (error) {
+        console.error(error);
     }
+  }
     fetchSpendings();
   }, []);
 
@@ -30,9 +35,8 @@ function WeeklySpendings() {
   return (
     <div style={boxStyles}>
       <p style={{ opacity: 0.6, padding: '8px 0px' }}>Your Total Spending (01.–07. Jan. 2022)</p>
-      {/*<p style={{ fontSize: '36px', lineHeight: '44px' }}>823.14€</p>*/}
-      <p style={{ fontSize: '36px', lineHeight: '44px' }}>{totalSpending} €</p>
-      {/*<p style={{ opacity: 0.6 }}>Ø117,59 € </p>*/}
+      {/*<p style={{ fontSize: '36px', lineHeight: '44px' }}>{totalSpending} €</p>*/}
+      <p className='display'>{totalSpending} €</p>
       <p style={{ opacity: 0.6 }}>Ø{dailyAverage} € per day </p>
       <div style={{ padding: '24px 0px 0px' }}>
         <BarChartComponent/>
